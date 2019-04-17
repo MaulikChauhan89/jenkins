76ab94763316b89e77f670190933008f355c7762
MaulikChauhan89 <49715039+MaulikChauhan89@users.noreply.github.com>
2019-04-17 15:21:22 +0530

    Create trainalg_pattern.py
    triangle pattern in python

---
+++ trainalg_pattern.py
@@ -0,0 +1,14 @@
+
+def pattern(n): 
+    myList = [] 
+    for i in range(1,n+1): 
+        myList.append("*"*i) 
+    print("\n".join(myList)) 
+  
+# Driver Code 
+n = 5
+pattern(n) 
+print("--------------------------------------------")
+print("--------------------Again-------------------")
+print("--------------------------------------------")
+pattern(n) 
